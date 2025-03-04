bf879b61571c8a9a121ea53c4546e7f23816b637
chaitanya7x <145201511+chaitanya7x@users.noreply.github.com>
2025-03-04 08:21:55 +0530

    Add files via upload

---
+++ q7.java
@@ -0,0 +1,11 @@
+//7	Java program to find ASCII value of a character
+public class q7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char a='a';
+int asciiValue=a;
+System.out.println(asciiValue);
+	}
+
+}
